89a8ec29c43f5b6cea0c4d222b6c123c54be03a0
hoxuannhatminh <hoxuannhatminh2000@gmail.com>
2021-11-11 21:49:48 +0700

    baikiemtra

---
+++ lib/helper/data.dart
@@ -0,0 +1,52 @@
+import 'package:news_app_api/models/categorie_model.dart';
+
+List<CategorieModel> getCategories(){
+
+  List<CategorieModel> myCategories = List<CategorieModel>();
+  CategorieModel categorieModel;
+
+  //1
+  categorieModel = new CategorieModel();
+  categorieModel.categorieName = "Business";
+  categorieModel.imageAssetUrl = "https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80";
+  myCategories.add(categorieModel);
+
+  //2
+  categorieModel = new CategorieModel();
+  categorieModel.categorieName = "Entertainment";
+  categorieModel.imageAssetUrl = "https://images.unsplash.com/photo-1572533416314-9fa71515b52a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80";
+  myCategories.add(categorieModel);
+
+  //3
+  categorieModel = new CategorieModel();
+  categorieModel.categorieName = "General";
+  categorieModel.imageAssetUrl = "https://images.unsplash.com/photo-1484100356142-db6ab6244067?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1067&q=80";
+  myCategories.add(categorieModel);
+
+  //4
+  categorieModel = new CategorieModel();
+  categorieModel.categorieName = "Health";
+  categorieModel.imageAssetUrl = "https://images.unsplash.com/photo-1517093911940-08cb5b3952e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=444&q=80";
+  myCategories.add(categorieModel);
+
+  //5
+  categorieModel = new CategorieModel();
+  categorieModel.categorieName = "Science";
+  categorieModel.imageAssetUrl = "https://images.unsplash.com/photo-1554475901-4538ddfbccc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80";
+  myCategories.add(categorieModel);
+
+  //5
+  categorieModel = new CategorieModel();
+  categorieModel.categorieName = "Sports";
+  categorieModel.imageAssetUrl = "https://images.unsplash.com/photo-1507668077129-56e32842fceb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80";
+  myCategories.add(categorieModel);
+
+  //5
+  categorieModel = new CategorieModel();
+  categorieModel.categorieName = "Technology";
+  categorieModel.imageAssetUrl = "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80";
+  myCategories.add(categorieModel);
+
+  return myCategories;
+
+}
---
+++ lib/secret.dart
@@ -0,0 +1,1 @@
+String apiKey = "7fd0687222aa4e5eaf705ac0c9481ca3";
